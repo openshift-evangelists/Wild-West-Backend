20c79dc7069e3bbb660c2ef26b3ee409c6e471d6
Grant Shipley <gshipley@gmail.com>
2017-09-21 13:15:33 -0230

    kwww

---
+++ src/test/java/com/openshift/kwww/KwwwApplicationTests.java
@@ -0,0 +1,16 @@
+package com.openshift.kwww;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KwwwApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
