e74bd6037fde7e6c1b8153f84ed8a293d20b22f9
gshipley <gshipley@gmail.com>
2017-09-24 12:29:38 -0400

    add model for OpenShift Objects

--- src/main/java/com/openshift/kwww/models/Game.java
+++ src/main/java/com/openshift/kwww/models/Game.java
@@ -1,11 +1,15 @@
 package com.openshift.kwww.models;
 
+import java.util.Hashtable;
+
 public class Game {
 
 	private Score score;
+	private Hashtable<String, OpenShiftObject> gameObjects;
 	
 	public Game() {
 		score = new Score();
+		gameObjects = new Hashtable<>();
 	}
 
 	public Score getScore() {
@@ -14,6 +18,16 @@ public class Game {
 
 	public void setScore(Score score) {
 		this.score = score;
+	}
+	
+	public void addGameObject(OpenShiftObject newObject) {
+		if(!gameObjects.containsKey(newObject.getObjectID())) {
+			gameObjects.put(newObject.getObjectID(), newObject);
+		}
+	}
+	
+	public void removeGameObject(OpenShiftObject theObject) {
+		gameObjects.remove(theObject.getObjectID());
 	}
 
 }
---
+++ src/main/java/com/openshift/kwww/models/OpenShiftObject.java
@@ -0,0 +1,20 @@
+package com.openshift.kwww.models;
+
+public class OpenShiftObject {
+
+	private String objectID;
+	private String objectType;
+	
+	public String getObjectID() {
+		return objectID;
+	}
+	public void setObjectID(String objectID) {
+		this.objectID = objectID;
+	}
+	public String getObjectType() {
+		return objectType;
+	}
+	public void setObjectType(String objectType) {
+		this.objectType = objectType;
+	}
+}
